129a5de6e3d61a8c0864a505a8d5d81e93aeb72e
0lch4 <127250399+0lch4@users.noreply.github.com>
2023-05-10 12:38:05 +0200

    skrypt ladujacy dane do tabeli

---
+++ ladowanie.py
@@ -0,0 +1,33 @@
+import os
+import mysql.connector
+from dotenv import load_dotenv
+
+load_dotenv()
+
+mydb = mysql.connector.connect(
+    host=os.getenv('host'),
+    user=os.getenv('user'),
+    password=os.getenv('password'),
+    database=os.getenv('database'),
+    charset='utf8mb4'
+)
+
+def open_file(filename):
+    return open(filename, 'r', encoding='utf-8')
+
+for filename in os.listdir("slowniki"):
+    if filename.endswith(".txt"):
+        tablename = os.path.splitext(filename)[0]
+
+        mycursor = mydb.cursor()
+        mycursor.execute("CREATE TABLE {} (slowo VARCHAR(255), tlumaczenie TEXT)".format(tablename))
+
+        with open_file(os.path.join("slowniki", filename)) as file:
+            for line in file:
+                slowo, tlumaczenie = line.strip().split(' - ', 1)
+                sql = "INSERT INTO {} (slowo, tlumaczenie) VALUES (%s, %s)".format(tablename)
+                val = (slowo, tlumaczenie)
+                mycursor.execute(sql, val)
+
+        mydb.commit()
+        print("Plik {} przetworzony".format(filename))
