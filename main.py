ae0671f3b07ad8a5169b1eb72fda49233b3aa762
0lch4 <127250399+0lch4@users.noreply.github.com>
2023-05-13 18:12:52 +0200

    skonczone widoki

--- main.py
+++ main.py
@@ -21,19 +21,27 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 async def show_form(request: Request):
     return templates.TemplateResponse("form.html", {"request": request})
 
-#widok wyswietlajacy nam odpowiedz na zapytanie, po otrzymaniu odpowiedzi mozna cofnac sie do widoku glownego
+
+
+#widok wyswietlajacy odpowiedz na zapytanie, po otrzymaniu odpowiedzi mozna cofnac sie do widoku glownego
 @app.post("/submit", response_class=HTMLResponse)
-async def submit_form(request: Request, words: str = Form(...)):
-    #rodzielam wynik tak aby wyswietlone byly tylko przetlumaczone slowa
-    queryy = query(words)
-    results_list = []
-    for q in queryy:
-        word = q[0]
-        results_list.append(word)
-    result = ', '.join(results_list)
-    if len(result)>0:
-        #w przypadku odnalezienia danych pokazuje tlumaczenie
-        return templates.TemplateResponse("result.html", {"request": request, "result": result})
+async def submit_form(request: Request, words: str = Form(None)):
+    if words is None:
+            #jesli uzytkownik nic nie poda zwraca strone glowna
+        return templates.TemplateResponse("form.html", {"request": request})
     else:
-        #w przypadku nie odnalezienia danych pokazuje komunikat o ich braku
-        return templates.TemplateResponse("unkown_word.html", {"request": request, "result": result})
+        #rodziela wynik tak aby wyswietlone byly tylko przetlumaczone slowa
+        queryy = query(words)
+        results_list = []
+        for q in queryy:
+            word = q[0]
+            results_list.append(word)
+        result = ', '.join(results_list)
+        if len(result)>0:
+            #w przypadku odnalezienia danych pokazuje tlumaczenie
+            return templates.TemplateResponse("result.html", {"request": request, "result": result})
+        else:
+            #w przypadku nie odnalezienia danych pokazuje komunikat o ich braku
+            return templates.TemplateResponse("unkown_word.html", {"request": request, "result": result})
+
+
